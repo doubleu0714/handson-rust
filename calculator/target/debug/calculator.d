484c4c6388869eb81856bb4c83367a10d1927b03
doubleu0714 <doubleu0714@Wonhyungui-MacBookPro.local>
2023-12-09 15:24:28 +0900

    Apply .gitignore

--- calculator/target/debug/calculator.d
+++
@@ -1,1 +0,0 @@
-/Users/doubleu0714/Documents/develop/handson/handson-rust/calculator/target/debug/calculator: /Users/doubleu0714/Documents/develop/handson/handson-rust/calculator/src/main.rs
--- calculator/target/debug/deps/calculator-cb02475e893842f3.d
+++
@@ -1,5 +0,0 @@
-/Users/doubleu0714/Documents/develop/handson/handson-rust/calculator/target/debug/deps/calculator-cb02475e893842f3: src/main.rs
-
-/Users/doubleu0714/Documents/develop/handson/handson-rust/calculator/target/debug/deps/calculator-cb02475e893842f3.d: src/main.rs
-
-src/main.rs:
