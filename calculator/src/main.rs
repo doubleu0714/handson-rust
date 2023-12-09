f9ac6ce6077647674810383b003f03752764a6b3
doubleu0714 <doubleu0714@Wonhyungui-MacBookPro.local>
2023-12-09 15:22:10 +0900

    gitignore 추가

--- calculator/src/main.rs
+++ calculator/src/main.rs
@@ -5,16 +5,20 @@ fn main() {
     let a1: i32 = args[1].parse::<i32>().unwrap();
     let operator: &String = &args[2];
     let a2: i32 = args[3].parse::<i32>().unwrap();
-    let result: i32 = if operator == "+" {
-        a1 + a2
-    } else if operator == "*" {
-        a1 * a2
-    } else if operator == "-" {
-        a1 - a2
-    } else if operator == "/" {
-        a1 / a2
-    } else {
-        panic!("not exist operator")
-    };
+    let result: i32 = calculate(a1, operator, a2).unwrap();
     println!("{} {} {} = {}", a1, operator, a2, result);
+}
+
+fn calculate(a1: i32, operator: &String, a2: i32) -> Result<i32, &str> {
+    if operator == "+" {
+        Ok(a1 + a2)
+    } else if operator == "*" {
+        Ok(a1 * a2)
+    } else if operator == "-" {
+        Ok(a1 - a2)
+    } else if operator == "/" {
+        Ok(a1 / a2)
+    } else {
+        Err("not exist operator")
+    }
 }
--- calculator/target/debug/deps/calculator-cb02475e893842f3.d
+++ calculator/target/debug/deps/calculator-cb02475e893842f3.d
@@ -1,5 +1,5 @@
-/Users/doubleu0714/Documents/develop/handson/rust/calculator/target/debug/deps/calculator-cb02475e893842f3: src/main.rs
+/Users/doubleu0714/Documents/develop/handson/handson-rust/calculator/target/debug/deps/calculator-cb02475e893842f3: src/main.rs
 
-/Users/doubleu0714/Documents/develop/handson/rust/calculator/target/debug/deps/calculator-cb02475e893842f3.d: src/main.rs
+/Users/doubleu0714/Documents/develop/handson/handson-rust/calculator/target/debug/deps/calculator-cb02475e893842f3.d: src/main.rs
 
 src/main.rs:
